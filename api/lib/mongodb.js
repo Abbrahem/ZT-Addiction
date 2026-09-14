3f585b6ea0da758ea7471b74c0f9b0048f9ce682
Abrahem Nasser <abrahemelgazaly2@gmail.com>
2026-09-14 22:29:39 +0300

    Add files via upload

--- api/lib/mongodb.js
+++ api/lib/mongodb.js
@@ -18,8 +18,16 @@ if (!uri) {
   throw new Error('Please add your MongoDB URI to .env file');
 }
 
-client = new MongoClient(uri, options);
-clientPromise = client.connect();
+if (process.env.NODE_ENV === 'production') {
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+} else {
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+}
 
 // Test connection
 clientPromise
